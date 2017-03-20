e2ce74e5db0f63adc2c238291f20d5c90c09f9b4
Al Amin Khan <amnkhan.me@gmail.com>
2017-03-21 00:43:25 +0600

    Silk Of Siam | Beta version
    Silk of siam beta 1

--- demo/fiverr/slick/assets/js/main.js
+++ demo/fiverr/slick/assets/js/main.js
@@ -1,3 +1,9 @@
+// Preloader
+$(window).load(function() {
+  "use strict";
+  $('#loader').fadeOut();
+});
+
 // Hero Product Carousel
 $('.hero-carousel').owlCarousel({
     loop:true,
@@ -18,4 +24,61 @@ $('.hero-carousel').owlCarousel({
             items:1
         }
     }
+});
+// Hero Product Carousel
+$('.product-carousel').owlCarousel({
+    loop:true,
+    nav:true,
+    navText: ['<i class="fa fa-angle-left"></i>' , '<i class="fa fa-angle-right"></i>'],
+    dots: false,
+    autoplay: true,
+    autoplayHoverPause: true,
+    autoplayTimeout: 3000,
+    items: 1,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+});
+
+// WOW
+var wow = new WOW({
+    //disabled for mobile
+    mobile: false
+});
+wow.init();
+
+// Contact Form
+$(document).on('submit', '#contactForm', function (e) {
+    e.preventDefault();
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var message = $("#message").val();
+    var dataString = 'name=' + name + '&email=' + email + '&message=' + message;
+    function isValidEmail(emailAddress) {
+        var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+        return pattern.test(emailAddress);
+    }
+    if (isValidEmail(email) && (message.length > 1) && (name.length > 1)) {
+        $.ajax({
+            type: "POST",
+            url: "../php/form-process.php",
+            data: dataString,
+            success: function () {
+                $('.success').fadeIn(1000);
+                $('.error').fadeOut(500);
+            }
+        });
+    } else {
+        $('.error').fadeIn(1000);
+        $('.success').fadeOut(500);
+    }
+    return false;
 });
